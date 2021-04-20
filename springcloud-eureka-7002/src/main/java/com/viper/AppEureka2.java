019d7d5a4b16698a3ab86b79e98ffb087c437a9d
viper <wyp3614@163.com>
2021-04-20 21:10:38 +0800

    springcloud-eureka-cluster1

--- springcloud-eureka-7002/src/main/java/com/viper/AppEureka2.java
+++ springcloud-eureka-7002/src/main/java/com/viper/AppEureka2.java
@@ -10,10 +10,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @date 2021/4/20
  */
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer  // 开启EurekaServer的注解
 public class AppEureka2 {
     public static void main(String[] args) {
         SpringApplication.run(AppEureka2.class, args);
-
     }
 }
